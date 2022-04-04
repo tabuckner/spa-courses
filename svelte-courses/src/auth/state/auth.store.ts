496974ec0904218cb96052a818678498e64fb165
Taylor Buckner <taylora.buckner@gmail.com>
2022-04-04 18:11:54 -0500

    (svelte): cleanup.

--- svelte-courses/src/auth/state/auth.store.ts
+++ svelte-courses/src/auth/state/auth.store.ts
@@ -3,10 +3,10 @@ import { MOCK_AUTH_TOKEN } from "../mocks/token.mock";
 import { MOCK_USER } from "../mocks/user.mock";
 import type { UserModel } from "../models/user.model";
 
-// export const isAuthenticated = writable(false);
-// export const authToken = writable(undefined);
-// export const currentUser = writable<UserModel>(undefined);
+export const isAuthenticated = writable(false);
+export const authToken = writable(undefined);
+export const currentUser = writable<UserModel>(undefined);
 
-export const isAuthenticated = writable(true);
-export const authToken = writable(MOCK_AUTH_TOKEN);
-export const currentUser = writable<UserModel>(MOCK_USER);
+// export const isAuthenticated = writable(true);
+// export const authToken = writable(MOCK_AUTH_TOKEN);
+// export const currentUser = writable<UserModel>(MOCK_USER);
