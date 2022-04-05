e2632fb4be1d0c8656324f26ee2474e56d7fa68c
Taylor Buckner <taylora.buckner@gmail.com>
2022-04-05 08:12:04 -0500

    (svelte): added bearer token logic.

--- svelte-courses/src/util/simulate-request.ts
+++ svelte-courses/src/util/simulate-request.ts
@@ -1,7 +1,15 @@
+import { get } from "svelte/store";
+import { authToken } from "../auth/state/auth.store";
+
 export const simulateRequest = <T>(functionToBeWrapped: () => T, delayTimeInMs = 200): Promise<T> => {
   return new Promise((res, rej) => {
     setTimeout(() => {
+      // TODO: Find a more performant approach. Not recommended for "hot" code paths.
+      const token = get(authToken);
+      // Add token to `Bearer` of request
+
       res(functionToBeWrapped())
     }, delayTimeInMs);
   });
 }
+  
